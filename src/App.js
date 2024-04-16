38a47d1f5154d8c0479b0a4ff60b7270e7f63071
Vivek Dubey <72728328+VivekDubey9@users.noreply.github.com>
2024-04-16 16:38:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App text-3xl text-green-500">
+      hello guys lets build moviematrix gpt!
+    </div>
+  );
+}
+
+export default App;
